2e4ef98fa3690fb18646ab92030fce78ae2574fc
Jouni Ala <sekrarange@gmail.com>
2020-12-22 21:38:26 +0200

    unicafe 1.11

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -9,7 +9,10 @@ const Button = ({text, handleClick}) => {
 
 const StatisticLine = ({text, value}) => {
   return (
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -21,12 +24,16 @@ const Statistics = ({good, neutral, bad}) => {
     return (
       <div>
         <h1>statistics</h1>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={total()} />
-        <StatisticLine text="average" value={(good - bad) / total()} />
-        <StatisticLine text="positive" value={good / total() * 100 + " %"} />
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total()} />
+            <StatisticLine text="average" value={(good - bad) / total()} />
+            <StatisticLine text="positive" value={good / total() * 100 + " %"} />
+          </tbody>
+        </table>
       </div>
     )
   }
