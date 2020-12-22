bd179d8ba745645506b06b981f01830932a5ab33
Jouni Ala <sekrarange@gmail.com>
2020-12-22 21:18:17 +0200

    unicafe 1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -5,15 +5,24 @@ const Statistics = ({good, neutral, bad}) => {
 
   const total = () => good + neutral + bad
 
+  if (total() > 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total()}</p>
+        <p>average {(good - bad) / total()}</p>
+        <p>positive {good / total() * 100} %</p>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total()}</p>
-      <p>average {(good - bad) / total()}</p>
-      <p>positive {good / total() * 100} %</p>
+      <p>No feedback given</p>
     </div>
   )
 }
