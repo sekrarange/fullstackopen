7a3cbc8b905c3fc366f8ed124c22e6259a17c7ba
Jouni Ala <sekrarange@gmail.com>
2020-12-22 21:11:15 +0200

    unicafe 1.7

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -11,6 +11,8 @@ const App = () => {
   const handleNeutral = () => setNeutral(neutral + 1)
   const handleBad = () => setBad(bad + 1)
 
+  const total = () => good + neutral + bad
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -21,6 +23,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total()}</p>
+      <p>average {(good - bad) / total()}</p>
+      <p>positive {good / total()} %</p>
     </div>
   )
 }
